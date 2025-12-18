e6098bf99338afcb95b5a76643e0c73be50abf8d
seb-larue96 <larue.sebastien@dsi-oi.com>
2025-12-18 10:30:36 +0400

    import features module into app module

--- apps/chatbot-proto/src/app/app.module.ts
+++ apps/chatbot-proto/src/app/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { FeaturesModule } from '../features/features.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [FeaturesModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
